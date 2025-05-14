2495b0b6ce5d5be8e0205cc5eda3370f2455ea3c
Rudolfs Rijkuris <rudolfs.rijkuris@gmail.com>
2025-05-14 17:43:11 +0300

    Initial Commit

---
+++ chilli_test_task/chilli_test_taskApp.swift
@@ -0,0 +1,17 @@
+//
+//  chilli_test_taskApp.swift
+//  chilli_test_task
+//
+//  Created by Rudolfs Rijkuris on 14/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct chilli_test_taskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
